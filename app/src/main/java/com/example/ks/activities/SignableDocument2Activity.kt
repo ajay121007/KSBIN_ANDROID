39113dc20df66b247a075aa19f4ee2a0a0ebbc34
Sanuria <sachin.sanuria.dev@gmail.com>
2020-10-06 21:05:46 +0530

    New Screens

---
+++ app/src/main/java/com/example/ks/activities/SignableDocument2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.ks.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ks.R
+
+class SignableDocument2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signable_document2)
+    }
+}
