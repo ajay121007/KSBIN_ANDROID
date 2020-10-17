e5a0a88275dab4b8322324aedb4fb1fcf1b0c557
Sanuria <sachin.sanuria.dev@gmail.com>
2020-10-17 16:42:14 +0530

    payment cards

---
+++ app/src/main/java/com/example/ks/activities/payment/MakePaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ks.activities.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ks.R
+
+class MakePaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_payment)
+    }
+}
