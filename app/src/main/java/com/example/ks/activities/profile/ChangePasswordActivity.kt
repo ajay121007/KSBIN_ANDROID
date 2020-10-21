5ccdad937ecbc276a55c84c3a5692e05c5929a5e
Sanuria <sachin.sanuria.dev@gmail.com>
2020-10-21 23:28:44 +0530

    Updated changes

---
+++ app/src/main/java/com/example/ks/activities/profile/ChangePasswordActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ks.activities.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.ks.R
+
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
