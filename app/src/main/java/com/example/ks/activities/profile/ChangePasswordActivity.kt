0f9c80f6503a534f25d535bfd9c37aac24d9f1e3
Sanuria <sachin.sanuria.dev@gmail.com>
2020-10-21 23:35:00 +0530

    change password

--- app/src/main/java/com/example/ks/activities/profile/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/ks/activities/profile/ChangePasswordActivity.kt
@@ -2,13 +2,18 @@ package com.example.ks.activities.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 import com.example.ks.R
+import com.example.ks.databinding.ActivityChangePasswordBinding
 
 
 class ChangePasswordActivity : AppCompatActivity() {
+
+    lateinit var activityChangePasswordBinding: ActivityChangePasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_password)
+        activityChangePasswordBinding=DataBindingUtil.setContentView(this, R.layout.activity_change_password)
+
     }
 }
